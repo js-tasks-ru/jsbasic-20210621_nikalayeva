48e78e2a92f1e0d8e9de603009908d9c382d85f0
Anastasiya Nikalayeva <nikalayeva@gmail.com>
2021-07-04 16:59:46 +0300

    module 3 task 2 added

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,9 @@
+let filteredArr = [];
 function filterRange(arr, a, b) {
-  // ваш код...
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= a && arr[i] <= b) {
+      filteredArr.push(arr[i]);
+    }
+  }
+  return filteredArr;
 }
