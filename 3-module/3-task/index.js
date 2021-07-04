acf4fc9a1741cae35d96ebff2e64edf19f14d145
Anastasiya Nikalayeva <nikalayeva@gmail.com>
2021-07-04 17:37:00 +0300

    module 3 task 3 added

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,16 @@
 function camelize(str) {
-  // ваш код...
+
+  let arr = str.split("-");
+  let strCamelCase;
+  let arrCamelCase = [arr[0]];
+
+
+     for(let i = 1; i < arr.length; i++) {
+       arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+       arrCamelCase.push(arr[i]);
+     }
+
+    strCamelCase = arrCamelCase.join('');
+
+  return strCamelCase;
 }
