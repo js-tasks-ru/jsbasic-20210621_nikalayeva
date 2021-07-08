1f57ccc4846a2be5b10de68dd07a0882381749e3
Anastasiya Nikalayeva <nikalayeva@gmail.com>
2021-07-08 12:16:05 +0300

    module 4 task 1 added

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
-function makeFriendsList(friends) {
-  // ваш код...
+function makeFriendsList (friends) {
+  let list = document.createElement('ul');
+  document.body.append(list);
+
+  friends.forEach(function(friend) {
+    item = document.createElement('li');
+    list.appendChild(item);
+    item.innerHTML = `${friend.firstName}` + ' ' + `${friend.lastName}`;
+
+})
+  return list;
 }
