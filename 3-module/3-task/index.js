bf22fe1646130958701b4a7bd61d467d9c115b3c
Anastasiya Nikalayeva <nikalayeva@gmail.com>
2021-07-11 13:56:00 +0300

    added fixes to module 3 task 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,4 +1,4 @@
 function camelize(str) {
   let arr = str.split("-");
-  return [arr[0]] + arr.filter((item, index) => index > 0).map(item => item[0].toUpperCase() + item.slice(1)).join('');
+  return arr[0] + arr.filter((item, index) => index > 0).map(item => item[0].toUpperCase() + item.slice(1)).join('');
 }
